7967b6c583c6d32b1c228cd6cb9cfe9493ff44f8
luke-owen-crowdhandler <60788788+luke-owen-crowdhandler@users.noreply.github.com>
2023-04-24 13:54:22 +0100

    Update index.js

--- examples/node-16-9-1/express-4-16-1/index.js
+++ examples/node-16-9-1/express-4-16-1/index.js
@@ -1,47 +1,22 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 const crowdhandler = require("crowdhandler-sdk");
 
-/* GET home page. */
-router.get("*", async function (req, res, next) {
+router.get("*", async (req, res) => {
+  const publicKey = "YOUR_PUBLIC_KEY";
+  const public_client = new crowdhandler.PublicClient(publicKey);
+  const ch_context = new crowdhandler.RequestContext(req, res);
+  const ch_gatekeeper = new crowdhandler.Gatekeeper(public_client, ch_context, publicKey);
+  const ch_status = await ch_gatekeeper.validateRequest();
 
-  //Found in crowdhandler control panel accounts -> api
-  let publicKey = "YOUR_PUBLIC_KEY"
-
-  //Initalize the client
-  let public_client = new crowdhandler.PublicClient(
-    publicKey,
-  );
-
-  //Initalize the request context
-  let ch_context = new crowdhandler.RequestContext(req, res);
-
-  //Initalize the gatekeeper
-  let ch_gatekeeper = new crowdhandler.Gatekeeper(
-    public_client,
-    ch_context,
-    publicKey,
-  );
-
-  //Validate the request
-  let ch_status = await ch_gatekeeper.validateRequest();
-
-  if (ch_status.setCookie) {
-    ch_gatekeeper.setCookie(ch_status.cookieValue);
-  }
-
-  if (ch_status.stripParams) {
-    ch_gatekeeper.redirectToCleanUrl(ch_status.targetURL);
-  }
+  ch_status.setCookie && ch_gatekeeper.setCookie(ch_status.cookieValue);
+  ch_status.stripParams && ch_gatekeeper.redirectToCleanUrl(ch_status.targetURL);
 
   if (!ch_status.promoted) {
     ch_gatekeeper.redirectIfNotPromoted();
   } else {
-
-    //YOUR APPLICATION CODE HERE
+    //YOUR CODE HERE
     res.render("index", { title: "hello" });
-
-    //Record the performance AFTER your application code has executed
     ch_gatekeeper.recordPerformance();
   }
 });
