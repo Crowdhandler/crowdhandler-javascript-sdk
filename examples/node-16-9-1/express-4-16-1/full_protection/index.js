0d6c4f416d085ef810c3f7980879abd66b375b3a
luke-owen-crowdhandler <60788788+luke-owen-crowdhandler@users.noreply.github.com>
2023-05-15 14:21:15 +0100

    Update index.js
    Provide a more sophisticated Express Framework example.

--- examples/node-16-9-1/express-4-16-1/full_protection/index.js
+++ examples/node-16-9-1/express-4-16-1/full_protection/index.js
@@ -2,23 +2,60 @@ const express = require("express");
 const router = express.Router();
 const crowdhandler = require("crowdhandler-sdk");
 
-router.get("*", async (req, res) => {
+// Middleware to handle CrowdHandler logic for all GET requests
+const crowdHandlerMiddleware = async (req, res, next) => {
   const publicKey = "YOUR_PUBLIC_KEY";
-  const public_client = new crowdhandler.PublicClient(publicKey);
-  const ch_context = new crowdhandler.RequestContext(req, res);
-  const ch_gatekeeper = new crowdhandler.Gatekeeper(public_client, ch_context, publicKey);
-  const ch_status = await ch_gatekeeper.validateRequest();
+  const publicClient = new crowdhandler.PublicClient(publicKey);
+  const chContext = new crowdhandler.RequestContext(req, res);
+  const chGatekeeper = new crowdhandler.Gatekeeper(publicClient, chContext);
 
-  ch_status.setCookie && ch_gatekeeper.setCookie(ch_status.cookieValue);
-  ch_status.stripParams && ch_gatekeeper.redirectToCleanUrl(ch_status.targetURL);
+  try {
+    const chStatus = await chGatekeeper.validateRequest();
 
-  if (!ch_status.promoted) {
-    ch_gatekeeper.redirectIfNotPromoted();
-  } else {
-    //YOUR CODE HERE
-    res.render("index", { title: "hello" });
-    ch_gatekeeper.recordPerformance();
+    if (chStatus.setCookie) {
+      chGatekeeper.setCookie(chStatus.cookieValue);
+    }
+
+    if (chStatus.stripParams) {
+      chGatekeeper.redirectToCleanUrl(chStatus.targetURL);
+    }
+
+    if (!chStatus.promoted) {
+      return chGatekeeper.redirectIfNotPromoted();
+    }
+
+    // If the request is promoted, save the chGatekeeper instance in res.locals for later use
+    res.locals.chGatekeeper = chGatekeeper;
+
+    // Continue to the next middleware or route handler
+    next();
+  } catch (err) {
+    // Error handling middleware
+    next(err);
   }
+};
+
+// Add the CrowdHandler middleware to the router
+router.use(crowdHandlerMiddleware);
+
+// Route handler for all paths
+router.get("*", (req, res, next) => {
+  // Render the view and send the HTML
+  res.render("index", { title: "hello" }, (err, html) => {
+    // Handle any errors during rendering
+    if (err) {
+      return next(err);
+    }
+
+    // Send the rendered HTML to the client
+    res.send(html);
+
+    // If the chGatekeeper instance exists in res.locals, record the performance
+    if (res.locals.chGatekeeper) {
+      res.locals.chGatekeeper.recordPerformance();
+    }
+  });
 });
 
+// Export the router
 module.exports = router;
